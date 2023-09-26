11c21a1087c59499a8716bd559df8bf00d2e6a77
Elefant78 <142886162+Elefant78@users.noreply.github.com>
2023-09-26 11:28:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Hello, World");
+
+string username;
+
+Console.Write("Wie lautet dein Name:");
+username = Console.ReadLine();
+Console.WriteLine("Hallo "  + username);
+
+string Tagesablauf;
+Console.Write("Wie war dein Tag?");
+Tagesablauf = Console.ReadLine();
+
+if (Tagesablauf == "gut");
+{
+    Console.WriteLine("Das ist schön zu hören.");
+}
+
+
+
+
+
+
+
+
+
+
